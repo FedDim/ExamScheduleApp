9481924d5aeecb51380873cc57b0c59e9640bcb4
Дмитрий <146709037+FedDim@users.noreply.github.com>
2025-11-18 19:13:25 +0300

    feat : загрузка базовых данных

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -324,7 +324,37 @@ namespace ExamScheduleApp
 
         private string GetFilePathFromData(string file)
         {
-            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", file);
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", file);
+            if (File.Exists(filePath)) return filePath;
+            else
+            {
+                filePath = string.Empty;
+                try
+                {
+                    string sourceFilePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "data", file));
+                    string targerDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+
+                    if (!Directory.Exists(targerDirectory)) Directory.CreateDirectory(targerDirectory);
+
+                    filePath = Path.Combine(targerDirectory, file);
+
+                    if (File.Exists(sourceFilePath))
+                    {
+                        File.Copy(sourceFilePath, filePath);
+                    }
+                    else
+                    {
+                        File.Create(filePath).Close();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка : {ex.Message}");
+                }
+
+                return filePath;
+            }
         }
     }
 }
